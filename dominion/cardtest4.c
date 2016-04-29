8984fc8ae6dbe92e8e0b942f0f1150a004da77f5
foulgerd <foulgerd@oregonstate.edu>
2016-04-28 18:59:43 -0700

    Adding cardtest files

---
+++ dominion/cardtest4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "dominion.h"
+
+/*
+ * THIS TESTS ADVENTURER
+ */
+int failed = 0;
+
+void assert_fail(int ans) {
+	if (ans == -1) {
+		printf("TEST FAILED\n");
+		failed = 1;
+	}
+}
+
+void assert_pass() {
+	if (!failed) {
+		printf("TEST PASSED\n");
+	}
+}
+
+int main() {
+	int result;
+	struct gameState s;
+
+        int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
+                outpost,baron,tribute};
+
+        initializeGame(2, k, 5, &s); 
+        
+        result = cardEffect(14, 1, 1, 1, &s, 3, 0);
+	
+	assert_fail(result == -1);
+	
+	assert_pass();
+	
+	return 0;
+}
+
